45483c52b5b83aa01a09b59e51006f2240591811
thabotr <1831661@students.wits.ac.za>
2020-04-09 01:30:11 +0000

    Added some bash scripts for automation. TODO- add scripts for entire android env setup or availability checks.

---
+++ moodle_install_1.sh
@@ -0,0 +1,39 @@
+#* Update aptitude repository info
+apt-get update
+
+#**Install Apache/MySQL/PHP
+apt install apache2 mysql-client mysql-server php libapache2-mod-php
+
+#**Install Additional Software
+apt install -y graphviz aspell ghostscript clamav php7.2-pspell php7.2-curl php7.2-gd php7.2-intl php7.2-mysql php7.2-xml php7.2-xmlrpc php7.2-ldap php7.2-zip php7.2-soap php7.2-mbstring
+
+#**Restart apache to load modules correctly
+service apache2 restart
+
+#**Install git
+apt install -y git
+
+#**Download moodle
+
+cd /opt
+#< download moodle code index
+git clone git://git.moodle.org/moodle.git
+
+cd moodle
+
+#< retrieve a list of each branch available
+git branch -a
+
+#< tell git to track latest stable version
+MOODLE_STABLE_VERSION="MOODLE_38_STABLE"
+git branch --track $MOODLE_STABLE_VERSION origin/$MOODLE_STABLE_VERSION
+
+#< checkout the specified version
+git checkout $MOODLE_STABLE_VERSION
+
+#**Copy local repo to /var/www/html/
+cp -R /opt/moodle /var/www/html/
+mkdir /var/moodledata
+chown -R www-data /var/moodledata
+chmod -R 777 /var/moodledata
+chmod -R 0755 /var/www/html/moodle
---
+++ set_env_vars.sh
@@ -0,0 +1,17 @@
+#android home
+export ANDROID_HOME=/home/tizzar/AndroidCLI/android-sdk-linux
+export PATH=$ANDROID_HOME/tools:$PATH
+export PATH=$ANDROID_HOME/tools/bin:$PATH
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/emulator:$PATH
+
+#android sdk root
+export ANDROID_SDK_ROOT=$ANDROID_HOME
+export PATH=$ANDROID_SDK_ROOT:$PATH
+
+#gradle
+export GRADLE_HOME=/home/tizzar/AndroidCLI/gradle/gradle-6.2.2
+export PATH=$GRADDLE_HOME/bin:$PATH
+
+#virtual device/emulator
+export ANDROID_AVD_HOME=/root/.android/avd
