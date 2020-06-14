1a1d98e7223c8bbe43446c7d21f4a7a64342f8c4
thabotr <1831661@students.wits.ac.za>
2020-06-14 13:40:49 +0000

    Added tests for feedback_provider lib and mock html files.

---
+++ marker_server_tests/feedbackProviderTest.php
@@ -0,0 +1,32 @@
+<?php
+require_once( "feedback_lib.php") ;
+use \PHPUnit\Framework\TestCase ;
+
+class feedbackProviderTest extends TestCase
+{
+	public static $fbp = null ;
+	public static $itr = null ;
+	public static $utr = null ;
+
+	public static function setUpBeforeClass() : void
+	{
+		self::$fbp = new feedback_provider() ;
+		self::$itr = new instrumented_tests_results( [ "tests/IT.html"]) ;
+		self::$utr = new unit_tests_results( [ "tests/UT.html"]) ;
+	}
+
+	public function testStubFuncSendFeedbackToMoodle() : void
+	{
+		$this->assertTrue( self::$fbp->send_feedback_to_moodle()) ;
+	}
+
+	public static function tearDownAfterClass() : void
+	{
+		self::$fbp = null ;
+		self::$itr = null ;
+		self::$utr = null ;
+	}
+}
+
+
+?>
